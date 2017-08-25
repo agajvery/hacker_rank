ad0fc3df2ddadc7fafe261abb0333d364232fd48
Gajvoronskij Aleksandr <antares.mips@gmail.com>
2017-08-25 17:43:30 +0300

    fix tests

--- algorithms/diagonal_difference/main.go
+++ algorithms/diagonal_difference/main.go
@@ -1,29 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"math"
+	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
-	var matrix [][]string
 	var i, j int64
 	var mainDiagonal, secondDiagonal int64
 	var result int64
 
-	// reader := bufio.NewReader(os.Stdin)
-	// strInputMatrixN, _ := reader.ReadString('\n')
-	// matrixN, error := strconv.ParseInt(strings.TrimRight(strInputMatrixN, "\n"), 10, 32)
+	matrix, matrixN := getInputParams()
 
-	// if error == nil {
-	// 	for idx := 0; int64(idx) < matrixN; idx++ {
-	// 		strInputItems, _ := reader.ReadString('\n')
-	// 		rowMatrix := strings.Split(strings.TrimRight(strInputItems, "\n"), " ")
-	// 		matrix = append(matrix, rowMatrix)
-	// 	}
-	// }
-	matrixN := 5
-	matrix = append(matrix, {"-10", "3", "0", "5", "4"})
 	for ; i < matrixN; i++ {
 		for j = 0; j < matrixN; j++ {
 			if i == j {
@@ -36,6 +29,44 @@ func main() {
 			}
 		}
 	}
-	result = (mainDiagonal - secondDiagonal) * -1
-	fmt.Printf("%v", result)
+	result = mainDiagonal - secondDiagonal
+	fmt.Printf("%v", math.Abs(float64(result)))
+}
+
+func getInputParams() (matrix [][]string, matrixN int64) {
+	reader := bufio.NewReader(os.Stdin)
+	strInputMatrixN, _ := reader.ReadString('\n')
+	matrixN, error := strconv.ParseInt(strings.TrimRight(strInputMatrixN, "\n"), 10, 32)
+
+	if error == nil {
+		for idx := 0; int64(idx) < matrixN; idx++ {
+			strInputItems, _ := reader.ReadString('\n')
+			rowMatrix := strings.Split(strings.TrimRight(strInputItems, "\n"), " ")
+			matrix = append(matrix, rowMatrix)
+		}
+	}
+	return
+}
+
+func getTestInputParams() (matrix [][]string, matrixN int64) {
+
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	scanner.Scan()
+	inputMatrixN := scanner.Text()
+
+	matrixN, _ = strconv.ParseInt(inputMatrixN, 10, 64)
+
+	for i := 0; int64(i) < matrixN; i++ {
+		scanner.Scan()
+		row := scanner.Text()
+		rowMatrix := strings.Split(strings.TrimRight(row, "\n"), " ")
+		matrix = append(matrix, rowMatrix)
+	}
+	return
 }
